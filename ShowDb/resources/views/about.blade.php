5051f6b2888ae354990ff101ce89d44ccbf72a0d
Paul Oehler <poehler@interworx.com>
2016-12-21 18:32:06 -0500

    Added about page content

--- ShowDb/resources/views/about.blade.php
+++ ShowDb/resources/views/about.blade.php
@@ -1,11 +1,75 @@
 @extends('layouts.master')
 
+@section('title')
+About
+@endsection
+
 @section('content')
 
 <div class="container">
-hi
-</div>
 
+  <div class="page-header">
+    <h2>What is this all about?</h2>
+  </div>
+  <div class="well">
+    <p>
+      Diehard fans of <a href="http://theavettbrothers.com">The Avett Brothers</a> tend to go to a lot of live shows.  A past time for many is keeping track of what shows they have attended, and what songs they've seen performed live, and when.  This site helps users keep track of that information very easily, and share it if they like.  Beyond that, it can act is a repository of interesting notes about Avett Brothers shows and songs, and links to video performances from shows.
+    </p>
+  </div>
+
+  <div class="page-header">
+    <h2>Where did this data come from?</h2>
+  </div>
+  <div class="well">
+    <p>
+      Historic show and setlist data provided by <a href="http://www.asmylifeturnstoasong.com">Tim Mossberger</a>.
+    </p>
+  </div>
+
+  <div class="page-header">
+    <h2>Why do some older shows have no or few songs listed?</h2>
+  </div>
+  <div class="well">
+    <p>
+      Data is spotty for some of the early shows.  If you can help fill in the gaps, contact one of the administrators!
+    </p>
+  </div>
+
+  <div class="page-header">
+    <h2>Who is in charge around here?</h2>
+  </div>
+  <div class="well">
+    <p>
+      The site is administered, maintained, and developed by Paul Oehler and Tim Mossberger.
+    </p>
+  </div>
+
+  <div class="page-header">
+    <h2>Where did this come from?</h2>
+  </div>
+
+  <div class="well">
+    <p>
+      This Avett Setlist Database and Website were conceived and created in December 2016 by Paul Oehler.
+    </p>
+  </div>
+
+  <div class="page-header">
+    <h2>Special Thanks</h2>
+  </div>
+
+  <div class="well">
+    <p>
+      <strong>Site Beta Testers</strong><br>
+      Alison, Tim, Caitlyn, Jen, Wayne, and Shannon
+    </p>
+    <p>
+      <strong>Also</strong><br>
+      Scott, Seth, Bob, Joe, Tania, Mike, Paul, Travis, Dane, Pete, Jim, Susie, and everyone else that plays any roll in making these shows happen.  You came to leave behind the world a better way.
+    </p>
+  </div>
+
+</div>
 
 
 @endsection
--- ShowDb/resources/views/user/index.blade.php
+++ ShowDb/resources/views/user/index.blade.php
@@ -8,6 +8,7 @@ Stats ({{ $user->username }})
 
 <div class="container">
   <h3>Stats ({{ $user->username }})</h3>
+  <div class="well">
   <dl class="dl-horizontal">
     <dt>Past Shows</dt>
     <dd><a href="{{ url()->current() }}/shows">{{ count($past_shows) }}</a></dd>
@@ -32,10 +33,11 @@ Stats ({{ $user->username }})
       @endif
     </dd>
   </dl>
-  <hr/>
+  </div>
   <h3>Yearly Breakdown</h3>
   @foreach($yearly_data as $str => $year)
   <h4>{{ $str }}</h4>
+  <div class="well">
   <dl class="dl-horizontal">
     <dt>Shows</dt>
     <dd><a href="{{ url()->current() }}/shows?q={{ $str }}">{{ $year->shows }}</a></dd>
@@ -43,8 +45,9 @@ Stats ({{ $user->username }})
     <dd><a href="{{ url()->current() }}/songs/?q={{ $str }}">{{ $year->unique_songs }}</a></dd>
     <dt>Song Performances</dt>
     <dd>{{ $year->songs }}</dd>
-    <hr/>
-    @endforeach
+  </dl>
+  </div>
+  @endforeach
 </div>
 
 @endsection
