5946dbd6dacd59db4a6b19d05a334eba31d9c9b5
Paul Oehler <poehler@interworx.com>
2016-12-20 20:22:35 -0500

    Admin user list display

--- ShowDb/resources/views/admin/users.blade.php
+++ ShowDb/resources/views/admin/users.blade.php
@@ -8,33 +8,38 @@ Admin
 
 <div class="container">
 
-<h2>Users</h2>
+  <h2>Users</h2>
 
-<table id="audittable" class="table table-striped">
-  <tbody>
-    <thead>
-      <th>ID</th>
-      <th>Avatar</th>
-      <th>Name</th>
-      <th>Username</th>
-      <th>Email</th>
-      <th>Shows</th>
-      <th>Created</th>
-    </thead>
-    @foreach($users as $user)
-    <tr>
-      <td>{{ $user->id }}</td>
-      <td><img width="50px" src="{{ $user->avatar}}"></td>
-      <td>{{ $user->name }}</td>
-      <td>{{ $user->username ?: "none"}}</td>
-      <td>{{ $user->email }}</td>
-      <td>{{ count($user->shows) }}</td>
-      <td>{{ $user->created_at }}</td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
-{!! $users->render() !!}
+  <table id="audittable" class="table table-striped">
+    <tbody>
+      <thead>
+	<th>ID</th>
+	<th>Avatar</th>
+	<th>Name</th>
+	<th>Username</th>
+	<th>Email</th>
+	<th>Shows</th>
+	<th>Created</th>
+      </thead>
+      @foreach($users as $user)
+      <tr>
+	<td>{{ $user->id }}</td>
+	<td><img width="50px" src="{{ $user->avatar}}"></td>
+	<td>
+	  {{ $user->name }}
+	  @if($user->admin)
+	  <strong>(admin)</strong>
+	  @endif
+	</td>
+	<td>{{ $user->username ?: "none"}}</td>
+	<td>{{ $user->email }}</td>
+	<td>{{ count($user->shows) }}</td>
+	<td>{{ $user->created_at }}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+  {!! $users->render() !!}
 
 </div>
 @endsection
