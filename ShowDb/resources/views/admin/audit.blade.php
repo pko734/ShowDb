ae3fa2b4367b703451f49ff0be2eca0f28b46449
Paul Oehler <poehler@interworx.com>
2016-12-24 14:47:04 -0500

    Fixed audit display

--- ShowDb/resources/views/admin/audit.blade.php
+++ ShowDb/resources/views/admin/audit.blade.php
@@ -20,7 +20,10 @@ Admin
 	@forelse($audits as $audit)
 	<tr>
 	  <td>{{ \Carbon\Carbon::createFromTimeStamp($audit->created_at->timestamp)->diffForHumans() }}</td>
-	  <td>{{ $audit->user->name }}</td>
+	  <td>
+	  @if( $audit->user )	  
+	  {{ $audit->user->name }}</td>
+	  @endif
 	  <td><a href="{{ $audit->route }}">{{ $audit->auditable_type }} {{ $audit->type }}</a></td>
 	  <td><pre>{{ json_encode(json_decode($audit->old), JSON_PRETTY_PRINT) }}</pre></td>
 	  <td><pre>{{ json_encode(json_decode($audit->new), JSON_PRETTY_PRINT) }}</pre></td>
