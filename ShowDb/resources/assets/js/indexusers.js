10a9e72889d239a02a2c37b3dc8de7868f5c3877
Paul Oehler <poehler@interworx.com>
2016-12-24 12:30:16 -0500

    tweak copy message

--- ShowDb/resources/assets/js/indexusers.js
+++ ShowDb/resources/assets/js/indexusers.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $cb = new Clipboard('#usershare');
     $cb.on('success', function(e) {
-	$('#usershare').attr('data-original-title', 'Copied!').tooltip('fixTitle').tooltip('show');
+	$('#usershare').attr('data-original-title', 'Copied to clipboard!').tooltip('fixTitle').tooltip('show');
     });
 
     $('[data-toggle="tooltip"]').tooltip();
