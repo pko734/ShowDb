2b319f9d7b00ae592d0c08a5c8eedf5aeeedf5a1
Paul Oehler <poehler@interworx.com>
2017-01-06 16:39:26 -0500

    template cleanup

--- ShowDb/routes/web.php
+++ ShowDb/routes/web.php
@@ -29,33 +29,33 @@ Route::get('data/songs', 'DataController@songs')->name('data.songs');
 Route::post(  'shows/{show}/notes',        'ShowController@storeNote')->name('show.notes.store');
 Route::delete('shows/{show}/notes/{note}', 'ShowController@destroyNote')->name('show.notes.destroy');
 
-Route::post(  'albums/{album}/notes',        'AlbumController@storeNote')->name('album.notes.store');
-Route::delete('albums/{album}/notes/{note}', 'AlbumController@destroyNote')->name('album.notes.destroy');
+Route::post('albums/{album}/notes', 'AlbumController@storeNote')->name('albums.notes.store');
+Route::delete('albums/{album}/notes/{note}', 'AlbumController@destroyNote')->name('albums.notes.destroy');
 
-Route::post(  'songs/{song}/notes',        'SongController@storeNote')->name('song.notes.store');
-Route::delete('songs/{song}/notes/{note}', 'SongController@destroyNote')->name('song.notes.destroy');
+Route::post('songs/{song}/notes', 'SongController@storeNote')->name('songs.notes.store');
+Route::delete('songs/{song}/notes/{note}', 'SongController@destroyNote')->name('songs.notes.destroy');
 
-Route::get('plays',              'SongController@findPlays')->name('song.plays.find');
-Route::get('songs/{song}/plays', 'SongController@showPlays')->name('song.plays.show');
+Route::get('plays', 'SongController@findPlays')->name('songs.plays.find');
+Route::get('songs/{song}/plays', 'SongController@showPlays')->name('songs.plays.show');
 
-Route::get('stats/{username}/songs/{song}/plays', 'UserController@showPlays')->name('user.song.plays.show');
+Route::get('stats/{username}/songs/{song}/plays', 'UserController@showPlays')->name('users.songs.plays.show');
 
 Route::post(  'setlistitems/{item}/video',         'ShowController@storeVideo')->name('show.video.store');
 Route::delete('setlistitems/{item}/video/{video}', 'ShowController@destroyVideo')->name('show.video.destroy');
 
-Route::post(  'albumitems/{item}/note',        'AlbumController@storeItemNote')->name('album.itemnote.store');
-Route::delete('albumitems/{item}/note/{note}', 'AlbumController@destroyItemNote')->name('album.itemnote.destroy');
+Route::post('albumitems/{item}/note', 'AlbumController@storeItemNote')->name('albums.itemnote.store');
+Route::delete('albumitems/{item}/note/{note}', 'AlbumController@destroyItemNote')->name('albums.itemnote.destroy');
 
 Route::get('admin',       'AdminController@index')->name('admin.index');
 Route::get('admin/audit', 'AdminController@audit')->name('admin.audit');
 Route::get('admin/users', 'AdminController@users')->name('admin.users');
 
-Route::put('songs/{song}/notes/{note}',         'SongController@updateNote')->name('song.note.update');
-Route::put('shows/{show}/notes/{note}',         'ShowController@updateNote')->name('show.note.update');
+Route::put('songs/{song}/notes/{note}', 'SongController@updateNote')->name('songs.note.update');
+Route::put('shows/{show}/notes/{note}', 'ShowController@updateNote')->name('shows.note.update');
 Route::put('setlistitems/{item}/video/{video}', 'ShowController@approveVideo')->name('show.video.approve');
 
-Route::put('albums/{album}/notes/{note}',     'AlbumController@updateNote')->name('album.note.update');
-Route::put('albumitems/{item}/video/{video}', 'AlbumController@approveItemNote')->name('album.itemnote.approve');
+Route::put('albums/{album}/notes/{note}', 'AlbumController@updateNote')->name('albums.note.update');
+Route::put('albumitems/{item}/video/{video}', 'AlbumController@approveItemNote')->name('albums.itemnote.approve');
 
 Route::get('stats/{username}',        'UserController@userstats')->name('user.stats');
 Route::get('stats/{username}/shows',  'UserController@shows')->name('user.shows');
