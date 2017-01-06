073e12f5051474645c20ecc3c2d146f628471ccf
Paul Oehler <poehler@interworx.com>
2017-01-06 16:35:35 -0500

    Enable Album menu item

--- ShowDb/resources/views/layouts/master.blade.php
+++ ShowDb/resources/views/layouts/master.blade.php
@@ -18,93 +18,91 @@
   <body>{!! Analytics::render() !!}
     <nav class="navbar navbar-default navbar-static-top">
       <div class="container">
-	<div class="navbar-header">
+    <div class="navbar-header">
 
-	  <!-- Collapsed Hamburger -->
-	  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-	    <span class="sr-only">Toggle Navigation</span>
-	    <span class="icon-bar"></span>
-	    <span class="icon-bar"></span>
-	    <span class="icon-bar"></span>
-	  </button>
+      <!-- Collapsed Hamburger -->
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+        <span class="sr-only">Toggle Navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
 
-	  <!-- Branding Image -->
-	  <a class="navbar-brand" href="{{ url('/') }}">
-	    {{ config('app.name', 'Laravel') }}:
-	  </a>
-	</div>
+      <!-- Branding Image -->
+      <a class="navbar-brand" href="{{ url('/') }}">
+        {{ config('app.name', 'Laravel') }}:
+      </a>
+    </div>
 
-	<div class="collapse navbar-collapse" id="app-navbar-collapse">
-	  <!-- Left Side Of Navbar -->
-	  <ul class="nav navbar-nav">
-	    @if(Auth::user())
-	    <li class="{{ isActiveUrl('/stats/' . Auth::user()->username) }}"><a href="/stats/{{ Auth::user()->username }}">My Stats</a></li>
-	    @endif
+    <div class="collapse navbar-collapse" id="app-navbar-collapse">
+      <!-- Left Side Of Navbar -->
+      <ul class="nav navbar-nav">
+        @if(Auth::user())
+        <li class="{{ isActiveUrl('/stats/' . Auth::user()->username) }}"><a href="/stats/{{ Auth::user()->username }}">My Stats</a></li>
+        @endif
 
-	    <li class="{{ isActiveUrl('/shows') }}"><a href="/shows">Shows</a></li>
-	    <li class="{{ isActiveUrl('/songs') }}"><a href="/songs">Songs</a></li>
-	    @if(Auth::user() && Auth::user()->admin)
-	    <li class="{{ isActiveUrl('/albums') }}"><a href="/albums">Albums</a></li>
-	    @endif
-	    <li class="{{ isActiveUrl('/about') }}">
-	      <a href="/about">About</a>
-	    </li>
-	    @if(Auth::user() && Auth::user()->admin)
-	    <li class="dropdown {{ isActiveRoute('admin.*') }}">
-	      <a class="dropdown-toggle" data-toggle="dropdown" href="#">Admin
-		<span class="caret"></span></a>
-	      <ul class="dropdown-menu">
-		<li class="{{ isActiveUrl('/admin')}}"><a href="/admin">Notes</a></li>
-		<li class="{{ isActiveUrl('/admin/users')}}"><a href="/admin/users">Users</a></li>
-		<li class="{{ isActiveUrl('/admin/audit')}}"><a href="/admin/audit">Audit</a></li>
-	      </ul>
-	    </li>
-	    @endif
+        <li class="{{ isActiveUrl('/shows') }}"><a href="/shows">Shows</a></li>
+        <li class="{{ isActiveUrl('/songs') }}"><a href="/songs">Songs</a></li>
+        <li class="{{ isActiveUrl('/albums') }}"><a href="/albums">Albums</a></li>
+        <li class="{{ isActiveUrl('/about') }}">
+          <a href="/about">About</a>
+        </li>
+        @if(Auth::user() && Auth::user()->admin)
+        <li class="dropdown {{ isActiveRoute('admin.*') }}">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Admin
+        <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+        <li class="{{ isActiveUrl('/admin')}}"><a href="/admin">Notes</a></li>
+        <li class="{{ isActiveUrl('/admin/users')}}"><a href="/admin/users">Users</a></li>
+        <li class="{{ isActiveUrl('/admin/audit')}}"><a href="/admin/audit">Audit</a></li>
+          </ul>
+        </li>
+        @endif
 
-	  </ul>
+      </ul>
 
-	  <!-- Right Side Of Navbar -->
-	  <ul class="nav navbar-nav navbar-right">
-	    <!-- Authentication Links -->
-	    @if (Auth::guest())
-	    <li><a href="{{ url('/auth/facebook') }}">Login with Facebook</a></li>
-	    <li><a href="{{ url('/login') }}">Login</a></li>
-	    <li><a href="{{ url('/register') }}">Register</a></li>
-	    @else
-	    <li class="dropdown">
-	      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-		@if(\Gravatar::exists(Auth::user()->email))
-		{!! \Gravatar::image(Auth::user()->email, 'test', ['width'=>25,'height'=>25]) !!}
-		@else
-		@if(Auth::user()->username)
-		{{ Auth::user()->username }}
-		@else
-		{{ Auth::user()->name }}
-		@endif
-		@endif
-		<span class="caret"></span>
-	      </a>
+      <!-- Right Side Of Navbar -->
+      <ul class="nav navbar-nav navbar-right">
+        <!-- Authentication Links -->
+        @if (Auth::guest())
+        <li><a href="{{ url('/auth/facebook') }}">Login with Facebook</a></li>
+        <li><a href="{{ url('/login') }}">Login</a></li>
+        <li><a href="{{ url('/register') }}">Register</a></li>
+        @else
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+        @if(\Gravatar::exists(Auth::user()->email))
+        {!! \Gravatar::image(Auth::user()->email, 'test', ['width'=>25,'height'=>25]) !!}
+        @else
+        @if(Auth::user()->username)
+        {{ Auth::user()->username }}
+        @else
+        {{ Auth::user()->name }}
+        @endif
+        @endif
+        <span class="caret"></span>
+          </a>
 
-	      <ul class="dropdown-menu" role="menu">
-		<li class="{{ isActiveUrl('/settings') }}">
-		  <a href="/settings">Settings</a>
-		</li>
-		<li>
-		  <a href="{{ url('/logout') }}"
-		     onclick="event.preventDefault();
-			      document.getElementById('logout-form').submit();">
-		    Logout
-		  </a>
+          <ul class="dropdown-menu" role="menu">
+        <li class="{{ isActiveUrl('/settings') }}">
+          <a href="/settings">Settings</a>
+        </li>
+        <li>
+          <a href="{{ url('/logout') }}"
+             onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();">
+            Logout
+          </a>
 
-		  <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-		    {{ csrf_field() }}
-		  </form>
-		</li>
-	      </ul>
-	    </li>
-	    @endif
-	  </ul>
-	</div>
+          <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+            {{ csrf_field() }}
+          </form>
+        </li>
+          </ul>
+        </li>
+        @endif
+      </ul>
+    </div>
       </div>
     </nav>
 
@@ -125,9 +123,9 @@
     @if (count($errors) > 0)
     <div class="alert alert-danger">
       <ul>
-	@foreach ($errors->all() as $error)
-	<li>{{ $error }}</li>
-	@endforeach
+    @foreach ($errors->all() as $error)
+    <li>{{ $error }}</li>
+    @endforeach
       </ul>
     </div>
     @endif
