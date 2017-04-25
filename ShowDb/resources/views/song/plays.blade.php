9a68207b80299011dba97924b5dca80faa22e6e4
Paul Oehler <poehler@interworx.com>
2017-04-25 16:35:32 -0400

    Fixed image rotation on song plays

--- ShowDb/resources/views/song/plays.blade.php
+++ ShowDb/resources/views/song/plays.blade.php
@@ -8,95 +8,95 @@ Plays
   <div class="panel-heading">
     <form action="/plays" method="GET" role="search">
       <label>
-        Find Shows Where A Song Was Played
-        @if(isset($user_restriction))
-        ({{ $user_restriction->username }})
-        @endif
+	Find Shows Where A Song Was Played
+	@if(isset($user_restriction))
+	({{ $user_restriction->username }})
+	@endif
       </label>
       <div id="plays-input" class="input-group">
-        <span class="ac-song-title">
-          <input name="title"
-          value="{{ $song->title }}"
-          class="form-control typeahead"
-          type="text"
-          placeholder="Song Title">
-          @if(isset($user_restriction))
-          <input type="hidden" name="username" value="{{ $user_restriction->username }}">
-          @endif
-        </span>
-        <span class="input-group-btn">
-          <button type="submit" class="btn btn-default">
-          <span class="glyphicon glyphicon-search"></span>
-          </button>
-        </span>
+	<span class="ac-song-title">
+	  <input name="title"
+	  value="{{ $song->title }}"
+	  class="form-control typeahead"
+	  type="text"
+	  placeholder="Song Title">
+	  @if(isset($user_restriction))
+	  <input type="hidden" name="username" value="{{ $user_restriction->username }}">
+	  @endif
+	</span>
+	<span class="input-group-btn">
+	  <button type="submit" class="btn btn-default">
+	  <span class="glyphicon glyphicon-search"></span>
+	  </button>
+	</span>
       </div>
     </form>
   </div><!--/.panel-heading-->
   <div class="is-table panel-body">
   <div class="is-table-col col-xs-3 image-col">
-    @include('widgets.slider', ['slides' => [
-    'http://thelstory.weebly.com/uploads/2/1/7/1/21710700/3103595.jpg']
-    ])
+      @include('widgets.slider', ['slides' =>
+	array_map(function($a){ return "/{$a}"; }, glob('sliderimages/*.jpg'))
+      ] )
   </div>
     <div class="is-table-col col-xs-9">
       <table id="showtable" class="table table-striped">
-        <thead>
-          <tr>
-            <th width="1px"></th>
-            <th>
-              <a href="{{ Request::fullUrlWithQuery(['d' => 'asc']) }}">
-                Date
-              </a>
-            </th>
-            <th>Venue</th>
-          </tr>
-        </thead>
-        <tbody>
-          @forelse($shows as $show)
-          <tr>
-            <td>
-              <span style="white-space:nowrap;">
-                @if($show->incomplete_setlist)
-                <i style="color: orange"
-                class="fa fa-exclamation"
-                data-toggle="tooltip"
-                data-placement="right"
-                title="Partial or incomplete setlist"
-                aria-hidden="true"></i>
-                @endif
-                @if($show->notes_count > 0)
-                <i class="fa fa-files-o"
-                data-toggle="tooltip"
-                data-placement="right"
-                title="{{ $show->notes_count }} notes"
-                aria-hidden="true"></i>
-                @endif
-              </span>
-            </td>
-            <td>{{ $show->date }}</td>
-            <td>
-              @if($user && $show->users->contains($user->id))
-              <a class="remove-show-link" data-show-id="{{ $show->id }}" title="Remove from my shows" href="">
-              <i style="color: green;" class="fa fa-check-square-o" aria-hidden="true"></i></a>
-              @elseif($user)
-              <a class="add-show-link" data-show-id="{{ $show->id }}" title="Add to my shows" href="">
-              <i style="color: green;" class="fa fa-square-o" aria-hidden="true"></i></a>
-              @endif
-              <a href="/shows/{{ $show->id }}">{{ $show->venue }}</a>
-            </td>
-          </tr>
-          @empty
-          <tr>
-            <td colspan="2">No matches</td>
-          </tr>
-          @endforelse
-        </tbody>
+	<thead>
+	  <tr>
+	    <th width="1px"></th>
+	    <th>
+	      <a href="{{ Request::fullUrlWithQuery(['d' => 'asc']) }}">
+		Date
+	      </a>
+	    </th>
+	    <th>Venue</th>
+	  </tr>
+	</thead>
+	<tbody>
+	  @forelse($shows as $show)
+	  <tr>
+	    <td>
+	      <span style="white-space:nowrap;">
+		@if($show->incomplete_setlist)
+		<i style="color: orange"
+		class="fa fa-exclamation"
+		data-toggle="tooltip"
+		data-placement="right"
+		title="Partial or incomplete setlist"
+		aria-hidden="true"></i>
+		@endif
+		@if($show->notes_count > 0)
+		<i class="fa fa-files-o"
+		data-toggle="tooltip"
+		data-placement="right"
+		title="{{ $show->notes_count }} notes"
+		aria-hidden="true"></i>
+		@endif
+	      </span>
+	    </td>
+	    <td>{{ $show->date }}</td>
+	    <td>
+	      @if($user && $show->users->contains($user->id))
+	      <a class="remove-show-link" data-show-id="{{ $show->id }}" title="Remove from my shows" href="">
+	      <i style="color: green;" class="fa fa-check-square-o" aria-hidden="true"></i></a>
+	      @elseif($user)
+	      <a class="add-show-link" data-show-id="{{ $show->id }}" title="Add to my shows" href="">
+	      <i style="color: green;" class="fa fa-square-o" aria-hidden="true"></i></a>
+	      @endif
+	      <a href="/shows/{{ $show->id }}">{{ $show->venue }}</a>
+	    </td>
+	  </tr>
+	  @empty
+	  <tr>
+	    <td colspan="2">No matches</td>
+	  </tr>
+	  @endforelse
+	</tbody>
       </table>
       </div><!--/.is-table-col-->
     </div><!--/.is-table-->
     <div class="panel-footer">
       <div class=" pull-right">
-        {!! $shows->render() !!}
+	{!! $shows->render() !!}
       </div>
       <div style="clear:both;"></div>
     </div>
