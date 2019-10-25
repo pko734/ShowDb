d527d734295329a77c5eb16509450a8fa5b8a783
Laravel Shift <shift@laravelshift.com>
2019-10-25 21:32:27 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them so you can review the commit diff for
    changes. Moving forward, you should use ENV variables or create a
    separate config file to allow the core config files to remain
    automatically upgradeable.

--- ShowDb/config/logging.php
+++ ShowDb/config/logging.php
@@ -4,6 +4,7 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
 
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Default Log Channel
@@ -14,7 +15,9 @@ return [
     | one of the channels defined in the "channels" configuration array.
     |
     */
+
     'default' => env('LOG_CHANNEL', 'stack'),
+
     /*
     |--------------------------------------------------------------------------
     | Log Channels
@@ -29,22 +32,27 @@ return [
     |                    "custom", "stack"
     |
     */
+
     'channels' => [
         'stack' => [
             'driver' => 'stack',
             'channels' => ['daily'],
+            'ignore_exceptions' => false,
         ],
+
         'single' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
+
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
             'days' => 14,
         ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
@@ -52,6 +60,7 @@ return [
             'emoji' => ':boom:',
             'level' => 'critical',
         ],
+
         'papertrail' => [
             'driver' => 'monolog',
             'level' => 'debug',
@@ -61,6 +70,7 @@ return [
                 'port' => env('PAPERTRAIL_PORT'),
             ],
         ],
+
         'stderr' => [
             'driver' => 'monolog',
             'handler' => StreamHandler::class,
@@ -69,13 +79,16 @@ return [
                 'stream' => 'php://stderr',
             ],
         ],
+
         'syslog' => [
             'driver' => 'syslog',
             'level' => 'debug',
         ],
+
         'errorlog' => [
             'driver' => 'errorlog',
             'level' => 'debug',
         ],
     ],
+
 ];
