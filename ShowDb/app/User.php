baf5e61aca97fbe7c475122d4512fa874ff3bb13
Laravel Shift <shift@laravelshift.com>
2019-10-25 21:32:41 +0000

    Shift cleanup

--- ShowDb/app/User.php
+++ ShowDb/app/User.php
@@ -20,8 +20,6 @@ class User extends Authenticatable
       'name', 'email', 'password', 'admin', 'avatar', 'fb_id', 'username', 'share',
     ];
 
-    
-
     /**
      * The attributes that should be hidden for arrays.
      *
